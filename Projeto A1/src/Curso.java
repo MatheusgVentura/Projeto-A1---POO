b52e629f03df3dba7adabd4cddfb212598058c80
Matheus ventura <163769497+MatheusgVentura@users.noreply.github.com>
2024-10-05 19:03:38 -0300

    Add files via upload

---
+++ Projeto A1/src/Curso.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Curso {
+    private String nome;
+    private List<Disciplina> disciplinas;
+
+    public Curso(String nome) {
+        this.nome = nome;
+        this.disciplinas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void adicionarDisciplina(Disciplina disciplina) {
+        disciplinas.add(disciplina);
+    }
+}
